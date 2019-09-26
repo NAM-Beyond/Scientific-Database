79f261a596189882239f888ebf9e183c9f5e1736
NAM-Beyond <n.bonnet@hotmail.fr>
2019-09-26 16:42:37 +0200

    - Went back to URL requests instead of storing local HTML file - Added a time sleep in order to avoid being flaged as a bot - Added a NoneType checker in order to avoid trying to find encode_contents() of a object that doesn't exists

--- Scientific-Database.py
+++ Scientific-Database.py
@@ -1,14 +1,23 @@
 from bs4 import BeautifulSoup
 import re
+import requests
+import time
 
 Database_SecondaryList = []
 
-for n in range(1, 11):
-    Database_Page = BeautifulSoup(open("Database_File" + str(n) + ".html"), "html.parser")
+for n in range(1, 100):
+    print("Page :" + str(n))
+    Database_URL = "https://scholar.google.com/scholar?start=" + str(n) + "0&q=%22schizophrenia%22+AND+(%22social+media%22OR+%22internet%22)+AND+(%22medical%22+OR+%22health%22)+AND+%22information%22&hl=fr&as_sdt=0,5&as_ylo=2006&as_yhi=2019"
+    Database_Response = requests.get(Database_URL)
+    Database_Page = BeautifulSoup(Database_Response.text, "html.parser")
     Database_PrimaryList = Database_Page.body.find_all("h3")
     for o in range(0, len(Database_PrimaryList)):
-        Database_PrimaryList[o] = Database_PrimaryList[o].find("a")
-        Database_SecondaryList.append(Database_PrimaryList[o].encode_contents().decode("UTF-8"))
+        if Database_PrimaryList[o].find("a") is None:
+            continue
+        else:
+            Database_PrimaryList[o] = Database_PrimaryList[o].find("a")
+            Database_SecondaryList.append(Database_PrimaryList[o].encode_contents().decode("UTF-8"))
+    time.sleep(10)
 
 for p in range(0, len(Database_SecondaryList)):
     Database_SecondaryList[p] = re.sub("<b>|</b>", "", Database_SecondaryList[p])
