ca1aa7c4700a5e7cb825d17b1bf4056fe3d1983f
NAM-Beyond <n.bonnet@hotmail.fr>
2019-10-01 17:42:29 +0200

    - Correcting search of captcha page to be more effective - Detecting end of search page - Finally wrote the gathering of all 3 files in one and detecting duplicates

--- Scientific-Database.py
+++ Scientific-Database.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 Database_PrimaryList = []
+Database_SecondaryList = []
+Database_TempList = []
 
 # Preparing Brave as Chrome, because using chromedriver
 options = webdriver.chrome.options.Options()
@@ -20,7 +22,8 @@ browser.find_element_by_name("q").send_keys("\"schizophrenia\" AND (\"social med
 time.sleep(3)
 # Clicking the search button
 browser.find_element_by_id("gs_hdr_tsb").click()
-element = WebDriverWait(browser, 240).until(EC.presence_of_element_located((By.ID, "gs_res_sb_yyc")))
+if browser.find_elements_by_xpath("//*[contains(@id,\"captcha\")]") != []: 
+    element = WebDriverWait(browser, 240).until(EC.presence_of_element_located((By.ID, "gs_res_sb_yyc")))
 time.sleep(3)
 # Clicking on selecting a specific range of dates
 browser.find_element_by_id("gs_res_sb_yyc").click()
@@ -32,31 +35,63 @@ time.sleep(3)
 browser.find_element_by_xpath("//*[@id=\"gs_res_sb_yyf\"]/div[2]/button").click()
 time.sleep(3)
 for n in range(0, 99):
-    element = WebDriverWait(browser, 240).until(EC.presence_of_element_located((By.TAG_NAME, "h3")))
+    if browser.find_elements_by_xpath("//*[contains(@id,\"captcha\")]") != []:
+        element = WebDriverWait(browser, 240).until(EC.presence_of_element_located((By.TAG_NAME, "h3")))
+    if  browser.find_elements_by_xpath("//*[contains(@id,\"captcha\")]") == [] and browser.find_element_by_xpath("//*[@id=\"gs_n\"]/center/table/tbody/tr/td[12]/a") == "" :
+        break
     for i in browser.find_elements_by_tag_name("h3"):
         Database_PrimaryList.append(i.text)
     browser.find_element_by_xpath("//*[@id=\"gs_n\"]/center/table/tbody/tr/td[12]/a").click()
     time.sleep(10)
-print(Database_PrimaryList)
+Database_PrimaryList.sort()
 with open("scholar.txt", "w+") as f:
     for i in Database_PrimaryList:
         if re.search("^\[[a-zA-Z]*\] ", i):
-            f.write(re.sub("^\[[a-zA-Z]*\] ", "", i))
+            f.write(re.sub("^\[[a-zA-Z]*\] ", "", i) + "\n")
         else:
-            f.write(i)
+            f.write(i + "\n")
 
 # PubMed Central results can be stored on a "Summary (Text)" file but need to be modified in order to have only the title on each line
-#with open("pmc_result.txt", "r") as f:
-#    lines = f.readlines()
-#with open("pmc.txt", "w+") as f:
-#    for line in lines:
-#        if re.search("^\d+: ", line):
-#            f.write(re.sub("^\d+:  ", "", line))
+with open("pmc_result.txt", "r") as f:
+    lines = f.readlines()
+    for line in lines:
+        if re.search("^\d+: ", line):
+            Database_TempList.append(re.sub("^\d+:  ", "", line))
+    Database_TempList.sort()
+with open("pmc.txt", "w+") as f:
+    for i in Database_TempList:
+        f.write(i)
 
 # PubMed results stored locally in a "Summary (Text)" format and treated in order to only display the title
-#with open("pubmed_result.txt", "r") as f:
-#    lines = f.readlines()
-#with open("pubmed.txt", "w+") as f:
-#    for line in lines:
-#        if re.search("TI  - ", line):
-#            f.write(re.sub("TI  - ", "", line))
+with open("pubmed_result.txt", "r") as g:
+    lines = g.readlines()
+    lines.sort()
+with open("pubmed.txt", "w+") as g:
+    for line in lines:
+        if re.search("TI  - ", line):
+            g.write(re.sub("TI  - ", "", line))
+
+# Gathering all results in one main file, with remove of duplicates
+with open("scholar.txt", "r") as h:
+    lines = h.readlines()
+    for line in lines:
+        Database_SecondaryList.append(line)
+with open("pmc.txt", "r") as l:
+    lines = l.readlines()
+    for line in lines:
+        Database_SecondaryList.append(line)
+with open("pubmed.txt", "r") as j:
+    lines = j.readlines()
+    for line in lines:
+        Database_SecondaryList.append(line)
+Database_SecondaryList.sort()
+n = 0
+for i in range(0, len(Database_SecondaryList) - 1):
+    if Database_SecondaryList[n] == Database_SecondaryList[n + 1]:
+        del Database_SecondaryList[n + 1]
+        n -= 1
+    else:
+        n += 1
+with open("database.txt", "w+") as k:
+    for i in Database_SecondaryList:
+        k.write(i)
